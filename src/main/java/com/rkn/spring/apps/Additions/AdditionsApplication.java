28e47f17429f0975ec5f78b172cbd12e9a34502c
Ravi Krishna Nittala <rkn@Ravis-MacBook-Pro.local>
2022-10-29 12:12:40 -0400

    Initial addition of Additions Project

---
+++ src/main/java/com/rkn/spring/apps/Additions/AdditionsApplication.java
@@ -0,0 +1,29 @@
+package com.rkn.spring.apps.Additions;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class AdditionsApplication {
+	
+	
+	@RequestMapping("/add")
+	public String prompt(@RequestParam String id1, @RequestParam String id2) {
+		//example url will look like http://localhost:8080/add?id1=1&id2=2
+		
+		int sum= Integer.parseInt(id1)+Integer.parseInt(id2);
+		return "The sum is: "+sum;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkn/spring/apps/Additions/AdditionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkn.spring.apps.Additions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
